3ae6c6f6a14529f7155854105a58a1314cba9c28
ankitsil170 <ankitsil170@gmail.com>
2026-03-05 15:27:33 +0530

    Remove build files and add proper gitignore

--- build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/deploy_Raytracer_22f710dad7.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/silan/CppProjects/Raytracer/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Raytracer-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/silan/CppProjects/Raytracer/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/Raytracer.exe"
-    GENERATE_QT_CONF
-)
